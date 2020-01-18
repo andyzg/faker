5d773d417e96f96d76b3f79ea3ef1c91e29efd0f
Andy Zhang <andzhng@gmail.com>
2020-01-18 15:59:43 -0800

    Socket io connection

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-app.run(debug=True)
+
+@socketio.on('test')
+def test_connection(data):
+    print(data)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
